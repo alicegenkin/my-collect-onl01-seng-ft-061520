c08c57808af6b27809ba4e787de09cb91be87d3a
Alice <agenkin1@gmail.com>
2020-06-26 04:02:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(collection)
   new_collection = []
   while counter <collection.length
     new_collection << yield(collection[counter])
-    couner+=1
+    counter+=1
   end
   new_collection 
 end
