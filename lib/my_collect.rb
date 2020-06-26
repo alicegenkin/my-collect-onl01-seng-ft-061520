c6389cd514a5995923a87353956a417b0e285541
Alice <agenkin1@gmail.com>
2020-06-26 04:01:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,9 @@
-def my_collect(collection) do |lang|
-  lang.upcase
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  while counter <collection.length
+    new_collection << yield(collection[counter])
+    couner+=1
+  end
+  new_collection 
 end
